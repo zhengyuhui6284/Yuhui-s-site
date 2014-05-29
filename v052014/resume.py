31160ba6f846114fc799f1dfac1c9b95ff3debc1
Yuhui <zhengyuhui6284@gmail.com>
2014-05-29 09:14:58 -0400

    v052014
    Personal Site hosted on GAE. Designed by Yuhui Zheng, May 2014.

---
+++ v052014/resume.py
@@ -0,0 +1,31 @@
+import cgi		#common gateway interface
+import urllib
+import os
+import jinja2	#templates
+import webapp2	#server templates/functions routing
+import datetime
+import json 	#serving as I/O of javascript functions
+import logging 	#for debugging
+
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__) + '/templates'),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+
+
+class MainPage(webapp2.RequestHandler):
+
+    def get(self):
+
+
+        template_values = {
+        }
+
+        template = JINJA_ENVIRONMENT.get_template('resume.html')
+        self.response.write(template.render(template_values))
+
+
+application = webapp2.WSGIApplication([
+    ('/resume', MainPage),
+], debug=True)
